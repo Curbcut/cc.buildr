16af6fea607dfebac857b615c1254782246d7b6f
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2022-10-04 11:22:33 -0400

    Start of building a dictionary for all different scales

---
+++ R/append_scale_to_dictionary.R
@@ -0,0 +1,29 @@
+#' Add a scale to the scales dictionary
+#'
+#' @param scales_dictionary <`data.frame`> An already built scales dictionary
+#' data.frame, most probably built using \code{\link[susbuildr]{census_scales_dictionary}}.
+#' @param scale <`character`> The scale code name, e.g. \code{"CSD"}..
+#' @param sing <`character`> The scale name in singular, e.g. \code{"city"}.
+#' @param plur <`character`> The scale name in plurial, e.g. \code{"cities"}.
+#' @param slider_title <`character`> The scale name used on the map slider,
+#' e.g. \code{"City"}.
+#' @param place_heading <`character`> The place heading on a selection, e.g.
+#' for "City of Laval" -> \code{"City of {name}"}.
+#' @param place_name <`character`> The place name on a selection as the first
+#' string of the dynamically generated text in the explore panel. e.g. for
+#' "Laval has a population of ..." -> \code{"{name}"}.
+#'
+#' @return The \code{scale_dictionary} data.frame that is fed with the
+#' additional row.
+#' @export
+append_scale_to_dictionary <- function(scales_dictionary, scale, sing, plur,
+                                       slider_title, place_heading,
+                                       place_name) {
+
+  # Add a new row
+  scales_dictionary[nrow(scales_dictionary) + 1,] <-
+    c(scale, sing, plur, slider_title, place_heading, place_name)
+
+  scales_dictionary
+
+}
---
+++ R/census_scales_dictionary.R
@@ -0,0 +1,40 @@
+#' Build census scales dictionary
+#'
+#' @param census_scales <`list`> A named list containing census scales built
+#' with the \code{\link[susbuildr]{build_census_scales}} function.
+#'
+#' @return A data.frame used as dictionary for texts on the platform.
+#' @export
+census_scales_dictionary <- function(census_scales) {
+
+  dict <-
+    data.frame(scale = c("CSD", "CT", "DA", "DB"),
+             sing = c(
+               "city",
+               "census tract",
+               "dissemination area",
+               "dissemination block"),
+             plur = c(
+               "cities",
+               "census tracts",
+               "dissemination areas",
+               "dissemination blocks"),
+             slider_title = c(
+               "City",
+               "Census tract",
+               "Dissemination area",
+               "Dissemination block"),
+             place_heading = c(
+               "{name_2} of {name}",
+               "Census tract {name} ({name_2})",
+               "Dissemination area {name} ({name_2})",
+               "Dissemination block {name} ({name_2})"),
+             place_name = c(
+               "{name}",
+               "Census tract {name}",
+               "Dissemination area {name}",
+               "Dissemination block {name}"))
+
+  dict[dict$scale %in% names(census_scales), ]
+
+}
